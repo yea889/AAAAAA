ccdfaf25d9fec3b4a9bf14a1316e26000b15c79a
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:54:16 -0600

    Initialize remove-alpha-feature-flag-step-publishing

---
+++ docs/docs/.vuepress/components/AlphaFeatureNotice.vue
@@ -0,0 +1,19 @@
+<style lang="stylus"></style>
+
+<template>
+  <div class="custom-block tip">
+    <p class="custom-block-title">ALPHA FEATURE</p>
+    <p>
+      This feature is in <strong>alpha</strong> and is subject to change.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlphaFeatureNotice",
+  props: {
+    feature: String,
+  },
+};
+</script>

63b372350a83f2251d1fd70af87de131ee041d48
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 20:04:35 -0600

    Initialize bug-sample_integer-option-zero

---
+++ components/pagerduty/sources/new-or-updated-incident/new-or-updated-incident.mjs
@@ -0,0 +1,27 @@
+import constants from "../../common/constants.mjs";
+import commonWebhook from "../common-webhook.mjs";
+
+export default {
+  ...commonWebhook,
+  key: "pagerduty-new-or-updated-incident",
+  name: "New or Updated Incident",
+  version: "0.1.0",
+  type: "source",
+  description: "Emit new event each time an incident is created or updated",
+  methods: {
+    ...commonWebhook.methods,
+    getMetadata(payload) {
+      return {
+        id: payload.id,
+        summary: `Incident ${payload.id} has been created or updated`,
+        ts: Date.parse(payload.occurred_at),
+      };
+    },
+    getEventTypes() {
+      return constants.INCIDENT_EVENT_TYPES;
+    },
+    getExtensionName() {
+      return "New or Updated Incident";
+    },
+  },
+};

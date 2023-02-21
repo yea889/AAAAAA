9e19532f3edc7922c1a9d059d5e466a391d8bc56
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:42:15 -0600

    Initialize airtable-action-example

---
+++ components/airtable/actions/create-single-record/create-single-record.js
@@ -0,0 +1,54 @@
+const airtable = require("../../airtable.app.js");
+const common = require("../common.js");
+
+module.exports = {
+  key: "airtable-create-single-record",
+  name: "Create single record",
+  description: "Adds a record to a table.",
+  version: "0.1.2",
+  type: "action",
+  props: {
+    ...common.props,
+    record: {
+      propDefinition: [
+        airtable,
+        "record",
+      ],
+    },
+    typecast: {
+      propDefinition: [
+        airtable,
+        "typecast",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const table = this.airtable.base(this.baseId)(this.tableId);
+
+    this.airtable.validateRecord(this.record);
+
+    if (!this.record.Feedback) {
+      console.log("No feedback in record, so not sending data to Airtable");
+      return;
+    }
+
+    const data = [
+      {
+        fields: this.record,
+      },
+    ];
+
+    const params = {
+      typecast: this.typecast,
+    };
+
+    try {
+      const [
+        response,
+      ] = await table.create(data, params);
+      return response;
+    } catch (err) {
+      this.airtable.throwFormattedError(err);
+    }
+  },
+};

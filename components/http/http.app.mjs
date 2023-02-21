275d55e3a8e1b4dbefc55f8bb48b12aa10cdd003
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 20:00:34 -0600

    Initialize remove-mit-9

---
+++ components/http/http.app.mjs
@@ -0,0 +1,11 @@
+export default {
+  type: "app",
+  app: "http",
+  propDefinitions: {
+    /* eslint-disable-next-line pipedream/props-description */
+    httpRequest: {
+      type: "http_request",
+      label: "HTTP Request Configuration",
+    },
+  },
+};

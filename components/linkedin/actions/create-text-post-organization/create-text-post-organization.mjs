43b90a2e18f4441a246924b670c94cd559938417
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:58:03 -0600

    Initialize license

---
+++ components/linkedin/actions/create-text-post-organization/create-text-post-organization.mjs
@@ -0,0 +1,37 @@
+import linkedin from "../../linkedin.app.mjs";
+
+export default {
+  key: "linkedin-create-text-post-organization",
+  name: "Create a Simple Post (Organization)",
+  description: "Create post on LinkedIn using text, URL or article. [See the docs](https://learn.microsoft.com/en-us/linkedin/marketing/integrations/community-management/shares/posts-api?view=li-lms-2022-11&tabs=http#create-organic-posts) for more information",
+  version: "0.0.2",
+  type: "action",
+  props: {
+    linkedin,
+    organizationId: {
+      propDefinition: [
+        linkedin,
+        "organizationId",
+      ],
+    },
+    text: {
+      propDefinition: [
+        linkedin,
+        "text",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const data = {
+      author: this.organizationId,
+      commentary: this.text,
+      visibility: "PUBLIC",
+    };
+    const response = await this.linkedin.createPost({
+      $,
+      data,
+    });
+    $.export("$summary", "Successfully created a new Post as Organization");
+    return response;
+  },
+};

5ae6531abfc494b8ea249cb12b359d6139974c42
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:54:59 -0600

    Initialize 4741-slack-user-groups-users-update

---
+++ components/pipedrive/sources/updated-deal/updated-deal.mjs
@@ -0,0 +1,38 @@
+import constants from "../../common/constants.mjs";
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "pipedrive-updated-deal",
+  name: "Updated Deal",
+  description: "Triggers when a deal is updated.",
+  version: "0.0.3",
+  type: "source",
+  dedupe: "greatest",
+  methods: {
+    ...common.methods,
+    getResourceFn() {
+      return this.pipedriveApp.getDeals;
+    },
+    getResourceFnArgs() {
+      return {
+        sort: "update_time DESC",
+      };
+    },
+    getResourceProperty() {
+      return "update_time";
+    },
+    getEventObject() {
+      return constants.EVENT_OBJECT.DEAL;
+    },
+    getEventAction() {
+      return constants.EVENT_ACTION.UPDATED;
+    },
+    getMetaId(resource) {
+      return this.getTimestamp(resource);
+    },
+    getTimestamp(resource) {
+      return Date.parse(resource.update_time);
+    },
+  },
+};
---
+++ components/shopify/sources/new-page/new-page.mjs
@@ -0,0 +1,36 @@
+import shopify from "../../shopify.app.mjs";
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+
+export default {
+  key: "shopify-new-page",
+  name: "New Page",
+  type: "source",
+  description: "Emit new event for each new page published.",
+  version: "0.0.9",
+  dedupe: "unique",
+  props: {
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+    shopify,
+  },
+  async run() {
+    const sinceId = this.db.get("since_id") || null;
+    let results = await this.shopify.getPages(sinceId);
+
+    for (const page of results) {
+      this.$emit(page, {
+        id: page.id,
+        summary: page.title,
+        ts: Date.now(),
+      });
+    }
+
+    if (results[results.length - 1])
+      this.db.set("since_id", results[results.length - 1].id);
+  },
+};

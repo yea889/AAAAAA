a2a1562bc079db42aa9456f571dff08c7a7e9929
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:42:05 -0600

    Initialize shopify-partner

---
+++ components/shopify_partner/sources/new_app_charges.mjs
@@ -0,0 +1,63 @@
+import common from "../common.mjs";
+import getAppTransactions from "../queries/getAppTransactions.mjs";
+
+export default {
+  key: "shopify_partner-new-app-charges",
+  name: "New App Charges",
+  type: "source",
+  version: "0.0.5",
+  description:
+    "Emit new events when new app charges made to your partner account.",
+  ...common,
+  props: {
+    ...common.props,
+    createdAtMin: {
+      type: "string",
+      description:
+        "Only include transactions after this specific time (ISO timestamp)",
+      label: "createdAtMin",
+      optional: true,
+    },
+    createdAtMax: {
+      type: "string",
+      description:
+        "Only include transactions up to this specific time (ISO timestamp)",
+      label: "createdAtMin",
+      optional: true,
+    },
+  },
+  async run() {
+    const {
+      createdAtMin,
+      createdAtMax,
+      cursor,
+      db,
+    } = this;
+
+    const variables = {
+      ...(createdAtMin || {}),
+      ...(createdAtMax || {}),
+      ...(cursor || {}),
+    };
+
+    await this.shopify.query({
+      db,
+      key: "shopify_partner-transactions",
+      query: getAppTransactions,
+      variables,
+      hasNextPagePath: "transactions.pageInfo.hasNextPage",
+      getCursor: (data) => {
+        const edges = data?.transactions?.edges || [];
+        return edges[0]?.cursor;
+      },
+      handleEmit: (data) => {
+        data.transactions.edges.map(({ node: { ...txn } }) => {
+          this.$emit(txn, {
+            id: txn.id,
+            summary: `Transaction ${txn.id} earned ${txn.netAmount.amount} ${txn.netAmount.currencyCode}`,
+          });
+        });
+      },
+    });
+  },
+};

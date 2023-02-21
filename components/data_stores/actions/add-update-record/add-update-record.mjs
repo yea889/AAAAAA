40b082a5fd87598c3eb87ca0b7a8649bad6c5eb0
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:45:46 -0600

    Initialize docs-node-export-data

---
+++ components/data_stores/actions/add-update-record/add-update-record.mjs
@@ -0,0 +1,47 @@
+import app from "../../data_stores.app.mjs";
+
+export default {
+  key: "data_stores-add-update-record",
+  name: "Add or update a single record",
+  description: "Add or update a single record in your [Pipedream Data Store](https://pipedream.com/data-stores/).",
+  version: "0.0.6",
+  type: "action",
+  props: {
+    app,
+    dataStore: {
+      propDefinition: [
+        app,
+        "dataStore",
+      ],
+    },
+    key: {
+      propDefinition: [
+        app,
+        "key",
+        ({ dataStore }) => ({
+          dataStore,
+        }),
+      ],
+    },
+    value: {
+      label: "Value",
+      type: "any",
+      description: "Enter a string, object, or array.",
+    },
+  },
+  async run({ $ }) {
+    const {
+      key,
+      value,
+    } = this;
+    const record = await this.dataStore.get(key);
+    const parsedValue = this.app.parseValue(value);
+    await this.dataStore.set(key, parsedValue);
+    // eslint-disable-next-line multiline-ternary
+    $.export("$summary", `Successfully ${record ? "updated the record for" : "added a new record with the"} key, \`${key}\`.`);
+    return {
+      key,
+      value,
+    };
+  },
+};

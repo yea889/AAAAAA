df68d5a8649e02b699721a3b2662280939e66e87
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 20:03:55 -0600

    Initialize feature/bitbucket-5114-2

---
+++ components/slack/sources/new-reaction-added/new-reaction-added.mjs
@@ -0,0 +1,88 @@
+import common from "../common/base.mjs";
+
+export default {
+  ...common,
+  key: "slack-new-reaction-added",
+  name: "New Reaction Added (Instant)",
+  version: "1.1.9",
+  description: "Emit new event when a member has added an emoji reaction to a message",
+  type: "source",
+  dedupe: "unique",
+  props: {
+    ...common.props,
+    conversations: {
+      propDefinition: [
+        common.props.slack,
+        "conversation",
+      ],
+      type: "string[]",
+      label: "Channels",
+      description: "Select one or more channels to monitor for new messages.",
+      optional: true,
+    },
+    // eslint-disable-next-line pipedream/props-description,pipedream/props-label
+    slackApphook: {
+      type: "$.interface.apphook",
+      appProp: "slack",
+      async eventNames() {
+        if (this.conversations?.length) {
+          const conversations = [];
+          for (const conversation of this.conversations) {
+            conversations.push(`reaction_added:${conversation}`);
+          }
+          return conversations;
+        }
+
+        return [
+          "reaction_added",
+        ];
+      },
+    },
+    ignoreBot: {
+      propDefinition: [
+        common.props.slack,
+        "ignoreBot",
+      ],
+    },
+    iconEmoji: {
+      propDefinition: [
+        common.props.slack,
+        "icon_emoji",
+      ],
+      description: "Select one or more emojis to use as a filter. E.g. `fire, email`",
+      type: "string[]",
+      optional: true,
+    },
+  },
+  methods: {
+    ...common.methods,
+    getSummary() {
+      return "New reaction added";
+    },
+    async processEvent(event) {
+      let iconEmojiParsed = [];
+
+      try {
+        iconEmojiParsed = typeof this.iconEmoji === "string" ?
+          JSON.parse(this.iconEmoji) :
+          this.iconEmoji;
+      } catch (error) {
+        iconEmojiParsed = this.iconEmoji.replace(/\s+/g, "").split(",");
+      }
+
+      if (
+        ((this.ignoreBot) && (event.subtype == "bot_message" || event.bot_id)) ||
+        (iconEmojiParsed?.length > 0 && !iconEmojiParsed.includes(event.reaction))
+      ) {
+        return;
+      }
+
+      event.message = await this.getMessage({
+        channel: event.item.channel,
+        event_ts: event.item.ts,
+      });
+
+      return event;
+    },
+  },
+};

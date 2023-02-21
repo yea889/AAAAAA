020c14bcefd116d825ca9f005ffb80827af2e56e
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:37:32 -0600

    Initialize discord-new-messages

---
+++ interfaces/http/examples/http-custom-status-code.js
@@ -0,0 +1,17 @@
+// Example component showing how to return a custom status code
+module.exports = {
+  name: "http-custom-status-code",
+  version: "0.0.1",
+  props: {
+    http: "$.interface.http",
+    status: "string"
+  },
+  async run(event) {
+    this.http.respond({
+      status: this.status
+    });
+    // Emit the status and the HTTP event, which contains
+    // the HTTP payload, headers, and more
+    this.$emit({ status: this.status, event });
+  }
+};

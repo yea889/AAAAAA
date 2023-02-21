4628326ad90d1ee5d5454497dcff6369b626afee
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 20:06:10 -0600

    Initialize process-street-new-components

---
+++ components/hubspot/sources/new-deal/new-deal.mjs
@@ -0,0 +1,48 @@
+import common from "../common.mjs";
+
+export default {
+  ...common,
+  key: "hubspot-new-deal",
+  name: "New Deals",
+  description: "Emit new event for each new deal created.",
+  version: "0.0.12",
+  dedupe: "unique",
+  type: "source",
+  hooks: {},
+  methods: {
+    ...common.methods,
+    getTs(deal) {
+      return Date.parse(deal.createdAt);
+    },
+    generateMeta(deal) {
+      const {
+        id,
+        properties,
+      } = deal;
+      const ts = this.getTs(deal);
+      return {
+        id,
+        summary: properties.dealname,
+        ts,
+      };
+    },
+    isRelevant(deal, createdAfter) {
+      return this.getTs(deal) > createdAfter;
+    },
+    getParams() {
+      return {
+        limit: 100,
+        sorts: [
+          {
+            propertyName: "createdate",
+            direction: "DESCENDING",
+          },
+        ],
+        object: "deals",
+      };
+    },
+    async processResults(after, params) {
+      await this.searchCRM(params, after);
+    },
+  },
+};

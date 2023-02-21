4f0cdbe5c6e5999a99a2ef521fbdfe8b10f82ae8
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:47:13 -0600

    Initialize 3254-dropbox-bug-listing-folder-options

---
+++ components/linear_app/sources/issue-updated-instant/issue-updated-instant.mjs
@@ -0,0 +1,43 @@
+import common from "../common/webhook.mjs";
+import constants from "../../common/constants.mjs";
+
+export default {
+  ...common,
+  key: "linear_app-issue-updated-instant",
+  name: "New Updated Issue (Instant)",
+  description: "Emit new event when an issue is updated. See the docs [here](https://developers.linear.app/docs/graphql/webhooks)",
+  type: "source",
+  version: "0.2.0",
+  dedupe: "unique",
+  methods: {
+    ...common.methods,
+    getResourceTypes() {
+      return [
+        constants.RESOURCE_TYPE.ISSUE,
+      ];
+    },
+    getWebhookLabel() {
+      return "Issue updated";
+    },
+    getActions() {
+      return [
+        constants.ACTION.UPDATE,
+      ];
+    },
+    getResourcesFn() {
+      return this.linearApp.listIssues;
+    },
+    getMetadata(resource) {
+      const {
+        delivery,
+        data,
+        updatedAt,
+      } = resource;
+      return {
+        id: delivery,
+        summary: `Issue Updated: ${data.title}`,
+        ts: Date.parse(updatedAt),
+      };
+    },
+  },
+};

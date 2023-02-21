ae9591b29a20c9c7b443b0873e8bca77bb8c5a9e
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:56:20 -0600

    Initialize whatsapp-actions-auth-changes

---
+++ components/hubspot/sources/new-form-submission/new-form-submission.mjs
@@ -0,0 +1,72 @@
+import common from "../common.mjs";
+
+export default {
+  ...common,
+  key: "hubspot-new-form-submission",
+  name: "New Form Submission",
+  description: "Emit new event for each new submission of a form.",
+  version: "0.0.10",
+  dedupe: "unique",
+  type: "source",
+  props: {
+    ...common.props,
+    forms: {
+      propDefinition: [
+        common.props.hubspot,
+        "forms",
+      ],
+      withLabel: false,
+    },
+  },
+  hooks: {},
+  methods: {
+    ...common.methods,
+    generateMeta(result) {
+      const {
+        pageUrl,
+        submittedAt: ts,
+      } = result;
+      const submitted = new Date(ts);
+      const id = pageUrl.split("/").pop();
+      return {
+        id: `${id}${ts}`,
+        summary: `Form submitted at ${submitted.toLocaleDateString()} ${submitted.toLocaleTimeString()}`,
+        ts,
+      };
+    },
+    isRelevant(result, submittedAfter) {
+      const relevant = result.submittedAt > submittedAfter;
+      if (relevant) {
+        this.updateAfter(result.submittedAt);
+      }
+      return relevant;
+    },
+    updateAfter(submittedAt) {
+      const after = this._getAfter();
+      if (submittedAt > after) {
+        this._setAfter(submittedAt);
+      }
+    },
+    getParams() {
+      return {
+        limit: 50,
+      };
+    },
+    async processResults(after, baseParams) {
+      await Promise.all(
+        this.forms
+          .map((form) => ({
+            ...baseParams,
+            formId: form,
+          }))
+          .map((params) =>
+            this.paginate(
+              params,
+              this.hubspot.getFormSubmissions.bind(this),
+              "results",
+              after,
+            )),
+      );
+    },
+  },
+};

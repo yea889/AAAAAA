c3e32b014b7d33d0256f84ff1b7007b58a9739d3
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:59:35 -0600

    Initialize remove-mit-3

---
+++ components/discord_bot/actions/list-channel-invites/list-channel-invites.mjs
@@ -0,0 +1,35 @@
+import constants from "../../constants.mjs";
+import common from "../common.mjs";
+
+const { discord } = common.props;
+
+export default {
+  ...common,
+  key: "discord_bot-list-channel-invites",
+  name: "List Channel Invites",
+  description: "Return a list of invitees for the channel. Only usable for guild channels.",
+  type: "action",
+  version: "0.0.7",
+  props: {
+    ...common.props,
+    channelId: {
+      propDefinition: [
+        discord,
+        "channelId",
+        ({ guildId }) => ({
+          guildId,
+          notAllowedChannels: [
+            ...constants.NOT_ALLOWED_CHANNELS,
+            constants.CHANNEL_TYPES.GUILD_CATEGORY,
+          ],
+        }),
+      ],
+    },
+  },
+  async run({ $ }) {
+    return this.discord.getChannelInvites({
+      $,
+      channelId: this.channelId,
+    });
+  },
+};
---
+++ components/http/actions/post-request/post-request.mjs
@@ -0,0 +1,20 @@
+import customRequest from "../custom-request/custom-request.mjs";
+
+export default {
+  ...customRequest,
+  key: "http-post-request",
+  name: "Send POST Request",
+  description: "Send an HTTP POST request to any URL. Optionally configure query string parameters, headers and basic auth.",
+  type: "action",
+  version: "1.0.1",
+  props: {
+    ...customRequest.props,
+    /* eslint-disable-next-line pipedream/props-label,pipedream/props-description */
+    httpRequest: {
+      ...customRequest.props.httpRequest,
+      default: {
+        method: "POST",
+      },
+    },
+  },
+};

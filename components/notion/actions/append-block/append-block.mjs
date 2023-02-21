025d07f0c27238b9c1da09d221ea9e17b8bbbb47
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:54:50 -0600

    Initialize all-polling-sources

---
+++ components/notion/actions/append-block/append-block.mjs
@@ -0,0 +1,88 @@
+import notion from "../../notion.app.mjs";
+import base from "../common/base-page-builder.mjs";
+
+export default {
+  ...base,
+  key: "notion-append-block",
+  name: "Append Block to Parent",
+  description: "Creates and appends blocks to the specified parent. [See the docs](https://developers.notion.com/reference/patch-block-children)",
+  version: "0.2.3",
+  type: "action",
+  props: {
+    notion,
+    pageId: {
+      propDefinition: [
+        notion,
+        "pageId",
+      ],
+      label: "Parent Block ID",
+      description: "The identifier for the parent block",
+    },
+    blockObjects: {
+      type: "string[]",
+      label: "Block Objects",
+      description: "This prop accepts an array of block objects to be appended. Using a custom expression in this prop is recommended.",
+      optional: true,
+    },
+    blockIds: {
+      propDefinition: [
+        notion,
+        "pageId",
+      ],
+      type: "string[]",
+      label: "Block IDs",
+      description: "Contents of selected blocks will be appended",
+      optional: true,
+    },
+    markupContents: {
+      type: "string[]",
+      label: "Markup Contents",
+      description: "Content of new blocks to append. You must use Markdown syntax [See docs](https://www.notion.so/help/writing-and-editing-basics#markdown-&-shortcuts)",
+      optional: true,
+    },
+  },
+  async run({ $ }) {
+    const blocks = [];
+    const children = [];
+    // add blocks from blockObjects
+    if (this.blockObjects?.length > 0) {
+      for (const obj of this.blockObjects) {
+        const child = (typeof obj === "string")
+          ? JSON.parse(obj)
+          : obj;
+        children.push(child);
+      }
+    }
+    // add blocks from blockIds
+    if (this.blockIds?.length > 0) {
+      for (const id of this.blockIds) {
+        const block = await this.notion.retrieveBlock(id);
+        const blockChildren = await this.notion.retrieveBlockChildren(block);
+        children.push(...blockChildren);
+      }
+    }
+    blocks.push(...children
+      .filter((child) => Object.keys(child[child.type]).length > 0 && child.type !== "child_page")
+      .map((child) => ({
+        object: "block",
+        type: child.type,
+        [child.type]: child[child.type],
+      })));
+
+    // add blocks from markup
+    if (this.markupContents?.length > 0) {
+      for (const content of this.markupContents) {
+        const block = this.createBlocks(content);
+        blocks.push(...block);
+      }
+    }
+
+    if (blocks.length === 0) {
+      $.export("$summary", "Nothing to append");
+      return;
+    }
+    const { results } = await this.notion.appendBlock(this.pageId, blocks);
+    $.export("$summary", `Appended ${results.length} block(s) successfully`);
+    return results;
+  },
+};
---
+++ components/shopify/sources/new-shipment/new-shipment.mjs
@@ -0,0 +1,44 @@
+import shopify from "../../shopify.app.mjs";
+import { DEFAULT_POLLING_SOURCE_TIMER_INTERVAL } from "@pipedream/platform";
+
+export default {
+  key: "shopify-new-shipment",
+  name: "New Shipment",
+  type: "source",
+  description: "Emit new event for each new fulfillment event for a store.",
+  version: "0.0.6",
+  dedupe: "unique",
+  props: {
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+      default: {
+        intervalSeconds: DEFAULT_POLLING_SOURCE_TIMER_INTERVAL,
+      },
+    },
+    shopify,
+  },
+  methods: {
+    emitShipments(results) {
+      for (const order of results) {
+        if (order.fulfillments && order.fulfillments.length > 0) {
+          for (const shipment of order.fulfillments) {
+            this.$emit(shipment, {
+              id: shipment.id,
+              summary: `Fulfillment ${shipment.name}`,
+              ts: Date.now(),
+            });
+          }
+        }
+      }
+    },
+  },
+  async run() {
+    this.emitShipments(
+      await this.shopify.getOrders("shipped"),
+    );
+    this.emitShipments(
+      await this.shopify.getOrders("partial"),
+    );
+  },
+};

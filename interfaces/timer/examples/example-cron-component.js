d8057ead8bf572cde20d605fd6437f40aaa24448
yea889 <125875552+yea889@users.noreply.github.com>
2023-02-20 19:37:23 -0600

    Initialize google-drive-changes-to-file

---
+++ interfaces/timer/examples/example-cron-component.js
@@ -0,0 +1,17 @@
+// Example component that runs a console.log() statement once a minute
+// Run `pd logs <component>` to see these logs in the CLI
+module.exports = {
+  name: 'cronjob',
+  version: '0.0.1',
+  props: {
+    timer: {
+      type: '$.interface.timer',
+      default: {
+        cron: '0 0 * * *',
+      },
+    },
+  },
+  async run() {
+    console.log('Run any Node.js code here');
+  },
+};
